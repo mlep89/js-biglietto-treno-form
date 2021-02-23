bbe1dec493a13f6e690edaaeb753d360a2432e5e
Marco <mr.lepore@hotmail.it>
2021-02-23 16:06:56 +0100

    pulizia file js

--- js/main.js
+++ js/main.js
@@ -49,39 +49,4 @@ function () {
 var btn_annulla = document.getElementById("annulla");
  
 
- //Verifica input
- /*
- if (isNaN(MyJourney)) {
-    alert("Rispondi utilizzando esclusivamente valori numerici");
-    
-    if (isNaN(MyJourney))
-    document.getElementById("MyTicket").innerHTML = ("Indicare la lunghezza del percorso esclusivamente in valori numerici ");
-}
- else {
-     console.log(ramoElse);
-
-     //Categorie biglietti
-     const bigliettostandard = Math.round((( 0.21 * MyJourney) + Number.EPSILON) * 100 / 100);
-     const bigliettoYoung = Math.round((( 0.21 * MyJourney * 0.8) + Number.EPSILON) * 100 / 100);
-     const bigliettoSenior = Math.round((( 0.21 * MyJourney * 0.6) + Number.EPSILON) * 100 / 100);
-
-
-     //Prezzo Bilgietto
-     if ((MyAge < 18)) {
-         document.getElementById("MyTicket").innerHTML = ("Il prezzo del biglietto è di €" + bigliettostandard);
-     }
-     
-     else if ((MyAge >= 18) && (MyAge <= 65)) {
-        document.getElementById("MyTicket").innerHTML = ("Il prezzo del biglietto è di € " + bigliettoYoung);
-     }
-
-     else ((MyAge > 65)) 
-        document.getElementById("MyTicket").innerHTML = ("Il prezzo del biglietto è di € " + bigliettoSenior);
-     
- }
-
- if (isNaN(MyAge)) {
-    alert("Rispondi utilizzando esclusivamente valori numerici");
-    
-    if (isNaN(MyAge))
-    document.getElementById("MyTicket").innerHTML = ("Indicare l'età in valori numerici "); */
+ 
